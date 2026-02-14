0c05e4778e7d9a62a0de2aef619098678c43c018
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-14 19:37:15 +0000

    creating admin panel for custom User model

--- users/admin.py
+++ users/admin.py
@@ -4,9 +4,25 @@ from .models import User
 
 # Register your models here.
 
+@admin.register(User)
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ['id', 'username', 'email', 'role']
     search_fields = ['id', 'username', 'email']
+    list_filter = ['role', 'is_staff']
+    ordering = ['id']
 
-admin.site.register(User, CustomUserAdmin)
+    fieldsets = (
+        (None, {'fields': ('email', 'username', 'password', 'role')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'username', 'password1', 'password2', 'role'),
+        }),
+    )
+
+
+
+    
