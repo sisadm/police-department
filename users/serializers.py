ebee1db97e4090a242f4e1bf819d48e64e0b4c1b
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-15 11:15:57 +0000

    finishing serializer for User

--- users/serializers.py
+++ users/serializers.py
@@ -1,1 +1,8 @@
-from django.core import serializers
+from rest_framework import serializers
+from .models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'email', 'username', 'role']
