22baebf32bb2110cf5e2f8e0c60ef8f87a5d7eb6
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-22 19:36:51 +0000

    creating urlpattern for registering user

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from users import views
+
+app_name = 'users'
+urlpatterns = [
+    path('register/', views.register, name='register'),
+]
