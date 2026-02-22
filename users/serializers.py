f34635bc5777b9edc6c7c479d3aa662dbf53be20
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-22 19:10:15 +0000

    userRegSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -7,10 +7,11 @@ class UserRegSerializer(serializers.ModelSerializer):
 
     username = serializers.RegexField(regex=r'^[A-Za-z][A-Za-z0-9._]*$',)
     email = serializers.EmailField()
+    password = serializers.CharField(write_only=True, min_length=8, required=True,)
 
     class Meta:
         model = User
-        fields = ['id', 'email', 'username', 'role']
+        fields = ['id', 'email', 'username', 'password', 'role']
 
     def validate_username(self, value):
         if User.objects.filter(username=value).exists():
@@ -21,6 +22,12 @@ class UserRegSerializer(serializers.ModelSerializer):
         if User.objects.filter(email=value).exists():
             raise serializers.ValidationError('Email already exists')
         return value
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 
