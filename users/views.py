6c7be9272a9555f6741d4f55d424b34c161ddb30
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-22 18:41:36 +0000

    api-view for POST request

--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import authentication, status
 from rest_framework.permissions import IsAuthenticated, AllowAny
@@ -10,15 +10,21 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from users.serializers import UserRegSerializer
 
 
-@api_view(['GET', 'POST', 'PUT', 'DELETE'])
-def UserView(request):
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register(request):
+    serializer = UserRegSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-
-
-    if request.method == 'POST':
-        serializer = UserRegSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# @api_view(['GET', 'POST', 'PUT', 'DELETE'])
+# def UserView(request):
+#
+#
+#     if request.method == 'POST':
+#         serializer = UserRegSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
