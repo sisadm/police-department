3ac323533d1895b49a3e9a19339225f1f4b062c6
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-22 21:12:01 +0000

    adding api token urls

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
 }
+
+
 
 # JWT Settings
 from datetime import timedelta
