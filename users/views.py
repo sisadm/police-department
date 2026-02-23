1dd3ede016a8b93b1c26a374f9b2df1d1c5c79e7
Adam Sisa <53064412+sisadm@users.noreply.github.com>
2026-02-23 21:38:51 +0000

    adding

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from rest_framework import authentication, status
+from rest_framework import authentication, status, viewsets
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -17,6 +17,13 @@ def register(request):
     serializer.is_valid(raise_exception=True)
     serializer.save()
     return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+
+
+
+
+
 
 # @api_view(['GET', 'POST', 'PUT', 'DELETE'])
 # def UserView(request):
